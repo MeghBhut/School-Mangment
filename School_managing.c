3657d7bcfca701e6525a9c7951ed47184f77df12
MeghBhut <meghbhut90@gmail.com>
2025-09-23 09:25:57 +0530

    Compelting all 10 functions

--- School_managing.c
+++ School_managing.c
@@ -18,58 +18,70 @@ struct Course{
 
 int main(){
     printf("Welcome to School Managing System."); // taking input action from user.
-    int choice;
-    printf("\n======================================================\n");
-    printf("Choose option From following: \n");
-    printf("1.Add New Student\n");
-    printf("2.Add New Course\n");
-    printf("3.View all Course\n");
-    printf("4.View all Student\n");
-    printf("5.Edit Course\n");
-    printf("6.Edit Student\n");
-    printf("7.Fee installment\n");
-    printf("8.Grades of Student\n");
-    printf("9.Remove student\n");
-    printf("10.Remove Course\n");
-    printf("0.Exit\n");
-    printf("Your choice: ");
-    scanf("%d",&choice);
-    printf("\n======================================================\n");
-    if(choice == 1)
+    while (1)
     {
-        Add_Student();
-    }
-    if(choice == 2)
-    {
-        Add_Course();
-    }
-    if(choice == 3)
-    {
-        View_All_Course();
-    }
-    if(choice == 4)
-    {
-        View_All_Student();
-    }
-    if(choice == 5)
-    {
-        Edit_Course();
-    }
-    if(choice == 6)
-    {
-        Edit_Student();
-    }
-    if(choice == 7)
-    {
-        Fee_Installment();
-    }
-    if(choice == 9)
-    {
-        Remove_Student();
-    }
-    if(choice == 10)
-    {
-        Remove_Course();
+        int choice;
+        printf("\n======================================================\n");
+        printf("Choose option From following: \n");
+        printf("1.Add New Student\n");
+        printf("2.Add New Course\n");
+        printf("3.View all Course\n");
+        printf("4.View all Student\n");
+        printf("5.Edit Course\n");
+        printf("6.Edit Student\n");
+        printf("7.Fee installment\n");
+        printf("8.Grades of Student\n");
+        printf("9.Remove student\n");
+        printf("10.Remove Course\n");
+        printf("0.Exit\n");
+        printf("Your choice: ");
+        scanf("%d",&choice);
+        printf("\n======================================================\n");
+        if(choice == 1)
+        {
+            Add_Student();
+        }
+        if(choice == 2)
+        {
+            Add_Course();
+        }
+        if(choice == 3)
+        {
+            View_All_Course();
+        }
+        if(choice == 4)
+        {
+            View_All_Student();
+        }
+        if(choice == 5)
+        {
+            Edit_Course();
+        }
+        if(choice == 6)
+        {
+            Edit_Student();
+        }
+        if(choice == 7)
+        {
+            Fee_Installment();
+        }
+        if(choice == 8)
+        {
+            Grades_of_Student();
+        }
+        if(choice == 9)
+        {
+            Remove_Student();
+        }
+        if(choice == 10)
+        {
+            Remove_Course();
+        }
+        if(choice == 0)
+        {
+            printf("Thanks for using our System!");
+            return 0;
+        }
     }
     return 0;
 }
@@ -78,8 +90,10 @@ void Add_Student()
 {
     //creating file if its not there.
     FILE *fs = fopen("student.dat","ab");
+    fclose(fs);
     FILE *fc = fopen("course.dat","ab");
-    FILE *temp = fopen("temp.dat","ab");
+    fclose(fc);
+    FILE *temp = fopen("temp.dat","wb");
     fs = fopen("student.dat","rb");
     fc = fopen("course.dat","rb");
     int No_of_student = 0; // No of student default value.
@@ -98,14 +112,14 @@ void Add_Student()
     printf("Enter Student Course name: ");
     fgets(s[No_of_student].course,sizeof(s[No_of_student].course),stdin);
     s[No_of_student].course[strcspn(s[No_of_student].course,"\n")] = '\0';
-    s[No_of_course].Marks = 0;
+    s[No_of_student].Marks = 0;
     //fees managment.
     int fees_given;
     for (int i = 0; i < No_of_course; i++)
     {
-        if(s[No_of_student].course==c[i].course_name)
+        if(strcmp(s[No_of_student].course,c[i].course_name)==0)
         {
-            printf("Fees need to pay for this course is: %d",c[i].course_name);
+            printf("Fees need to pay for this course is: %d\n",c[i].course_fee);
             printf("Enter Fees Payed: ");
             scanf("%d",&fees_given);
             if(fees_given==0)
@@ -125,9 +139,17 @@ void Add_Student()
         fwrite(&c[i],sizeof(struct Course),1,temp);
     }
     fclose(temp);
-    remove("course.dat");
-    rename("temp.dat","course.dat");
     fclose(fc);
+    fclose(fs);
+     if (remove("course.dat") != 0) {
+        printf("Error deleting old course.dat file.\n");
+    } else {
+        if (rename("temp.dat", "course.dat") != 0) {
+            printf("Error renaming temp.dat to course.dat.\n");
+        } else {
+            printf("Course data updated successfully.\n");
+        }
+    }
     //writing new arry element in file.
     fs = fopen("student.dat","ab");
     fwrite(&s[No_of_student],sizeof(struct Student),1,fs);
@@ -139,30 +161,37 @@ void Add_Course()
 {
     //creating file if its not there.
     FILE *fc = fopen("course.dat","ab");
+    fclose(fc);
     fc = fopen("course.dat","rb");
     int No_of_course = 0; // No of course default value.
     struct Course c[100]; // making arry of course structure.
     No_of_course = fread(c,sizeof(struct Course),100,fc); // reading from file and storing no of course value.
     //Taking User inputs.
+    getchar();
     printf("Enter New Course name: ");
     fgets(c[No_of_course].course_name,sizeof(c[No_of_course].course_name),stdin);
     c[No_of_course].course_name[strcspn(c[No_of_course].course_name,"\n")]='\0';
     printf("Enter Course Fee: ");
-    scanf("%d",c[No_of_course].course_fee);
+    scanf("%d",&c[No_of_course].course_fee);
     c[No_of_course].strength =0;
+    fclose(fc);
+    fc = fopen("course.dat","ab");
     fwrite(&c[No_of_course],sizeof(struct Course),1,fc);
     fclose(fc);
+    printf("Course Created Sucessfully!");
 }
 
 void View_All_Course()
 {
     //creating file if its not there.
     FILE *fc = fopen("course.dat","ab");
+    fclose(fc);
     fc = fopen("course.dat","rb");
     int No_of_course = 0; // No of course default value.
     struct Course c[100]; // making arry of course structure.
     No_of_course = fread(c,sizeof(struct Course),100,fc); // reading from file and storing no of course value.
     //Printing Output.
+    printf("%d",No_of_course);
     printf("\n------------------------------------------------------------\n");
     printf("%-10s | %-10s | %-5s","Course Name","Course Fee","Strength");
     printf("\n------------------------------------------------------------\n");
@@ -170,6 +199,7 @@ void View_All_Course()
     {
         printf("%-10s | %-10d | %-5d",c[i].course_name,c[i].course_fee,c[i].strength);
     }
+    printf("\n");
     fclose(fc);
 }
 
@@ -177,6 +207,7 @@ void View_All_Student()
 {
     //creating file if its not there.
     FILE *fs = fopen("student.dat","ab");
+    fclose(fs);
     fs = fopen("student.dat","rb");
     int No_of_student = 0; // No of course default value.
     struct Student s[100]; // making arry of course structure.
@@ -189,6 +220,7 @@ void View_All_Student()
     {
         printf("%-10s | %-10s | %-5d",s[i].name,s[i].course,s[i].Fee_remaining);
     }
+    printf("\n");
     fclose(fs);
 }
 
@@ -196,6 +228,7 @@ void Edit_Course()
 {
     //creating file if its not there.
     FILE *fc = fopen("course.dat","ab");
+    fclose(fc);
     FILE *temp = fopen("temp.dat","ab");
     fc = fopen("course.dat","rb");
     int No_of_course = 0; // No of course default value.
@@ -204,6 +237,7 @@ void Edit_Course()
     No_of_course = fread(c,sizeof(struct Course),100,fc); // reading from file and storing no of course value.
     View_All_Course();
     //Editing Course.
+    getchar();
     printf("Enter Course name You want to Edit: ");
     fgets(edit_course_name,sizeof(edit_course_name),stdin);
     edit_course_name[strcspn(edit_course_name,"\n")]='\0';
@@ -252,14 +286,16 @@ void Edit_Student()
 {
     //creating file if its not there.
     FILE *fs = fopen("student.dat","ab");
+    fclose(fs);
     FILE *temp = fopen("temp.dat","ab");
     fs = fopen("student.dat","rb");
     int No_of_student = 0; // No of student default value.
     char edit_student_name[50];
     struct Student s[100]; // making arry of student structure.
     No_of_student = fread(s,sizeof(struct Student),100,fs); // reading from file and storing no of student value.
-    View_All_Course();
+    View_All_Student();
     //Editing Course.
+    getchar();
     printf("Enter Student name You want to Edit: ");
     fgets(edit_student_name,sizeof(edit_student_name),stdin);
     edit_student_name[strcspn(edit_student_name,"\n")]='\0';
@@ -308,14 +344,16 @@ void Grades_of_Student()
 {
     //creating file if its not there.
     FILE *fs = fopen("student.dat","ab");
+    fclose(fs);
     FILE *temp = fopen("temp.dat","ab");
     fs = fopen("student.dat","rb");
     int No_of_student = 0; // No of student default value.
     char edit_student_name[50];
     struct Student s[100]; // making arry of student structure.
     No_of_student = fread(s,sizeof(struct Student),100,fs); // reading from file and storing no of student value.
-    View_All_Course();
+    View_All_Student();
     //Editing Course.
+    getchar();
     printf("Enter Student name You want to add marks: ");
     fgets(edit_student_name,sizeof(edit_student_name),stdin);
     edit_student_name[strcspn(edit_student_name,"\n")]='\0';
@@ -323,7 +361,8 @@ void Grades_of_Student()
     {
         if(strcmp(edit_student_name,s[i].name)==0)
         {
-            
+            printf("Enter marks(out of 100): ");
+            scanf("%d",&s[i].Marks);
             fwrite(&s[i],sizeof(struct Student),1,temp);
         }
     }
@@ -338,6 +377,7 @@ void Remove_Course()
 {
     //creating file if its not there.
     FILE *fc = fopen("course.dat","ab");
+    fclose(fc);
     FILE *temp = fopen("temp.dat","ab");
     fc = fopen("course.dat","rb");
     int No_of_course = 0; // No of course default value.
@@ -346,6 +386,7 @@ void Remove_Course()
     No_of_course = fread(c,sizeof(struct Course),100,fc); // reading from file and storing no of course value.
     View_All_Course();
     //Editing Course.
+    getchar();
     printf("Enter Course name You want to remove: ");
     fgets(remove_course_name,sizeof(remove_course_name),stdin);
     remove_course_name[strcspn(remove_course_name,"\n")]='\0';
@@ -378,7 +419,9 @@ void Remove_Student()
 {
     //creating file if its not there.
     FILE *fs = fopen("student.dat","ab");
+    fclose(fs);
     FILE *fc = fopen("course.dat","ab");
+    fclose(fc);
     FILE *temp = fopen("temp.dat","ab");
     FILE *temp2 = fopen("temp2.dat","ab");
     fs = fopen("student.dat","rb");
@@ -392,6 +435,7 @@ void Remove_Student()
     No_of_course = fread(c,sizeof(struct Course),100,fc); // reading from file and storing no of course value.
     View_All_Student();
     //Editing Course.
+    getchar();
     printf("Enter Student name You want to remove: ");
     fgets(remove_student_name,sizeof(remove_student_name),stdin);
     remove_student_name[strcspn(remove_student_name,"\n")]='\0';
@@ -406,7 +450,7 @@ void Remove_Student()
             {
                 for (int j = 0; j < No_of_course; j++)
                 {
-                    if(s[i].course==c[j].course_name)
+                    if(strcmp(s[i].course,c[j].course_name)==0)
                     {
                         c[j].strength--;
                     }
@@ -439,7 +483,10 @@ void Fee_Installment()
 {
     //creating file if its not there.
     FILE *fs = fopen("student.dat","ab");
+    fclose(fs);
     FILE *fc = fopen("course.dat","ab");
+    fclose(fc);
+    FILE *temp = fopen("temp.dat","wb");
     fs = fopen("student.dat","rb");
     fc = fopen("course.dat","rb");
     int No_of_student = 0; // No of student default value.
@@ -457,9 +504,9 @@ void Fee_Installment()
     {
         if(Filter_student_id==s[i].id)
         {
-            printf("Name of student is: %s",s[i].name);
-            printf("Fees Already Payed is: %s",s[i].Fee_payed);
-            printf("Fees remaining to give is: %s",s[i].Fee_remaining);
+            printf("Name of student is: %s\n",s[i].name);
+            printf("Fees Already Payed is: %d\n",s[i].Fee_payed);
+            printf("Fees remaining to give is: %d\n",s[i].Fee_remaining);
             printf("Want To procced intallment %d[y/n]",s[i].installment);
             char procced_choice;
             scanf(" %c",&procced_choice);
@@ -470,19 +517,25 @@ void Fee_Installment()
                 scanf("%d",&fees_given);
                 for (int j = 0; j < No_of_course; j++)
                 {
-                    if(s[i].course==c[j].course_name)
+                    if(strcmp(s[i].course,c[j].course_name)==0)
                     {
                         s[i].Fee_payed += fees_given;
-                        s[i].Fee_remaining = c[i].course_fee-s[i].Fee_payed;
+                        s[i].Fee_remaining = c[j].course_fee-s[i].Fee_payed;
                         s[i].installment++;
                         if(s[i].Fee_remaining==0)
                         {
-                            printf("Congratulation YOur all fees Payed.");
+                            printf("Congratulation YOur all fees Payed.\n");
                         }
                     }
+                    fwrite(&s[i],sizeof(struct Student),1,temp);
                 }
                 printf("Fee Installment updated.");
             }
         }
     }
+    fclose(temp);
+    fclose(fs);
+    fclose(fc);
+    remove("student.dat");
+    rename("temp.dat","student.dat");
 }
